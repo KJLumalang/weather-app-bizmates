a1c0b35cb56d92772b14d7c7f3e6898e0ed1394d
Kim Jeiro Lumalang <102845013+KJLumalang@users.noreply.github.com>
2024-10-01 10:04:03 +0800

    Removed an image

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,12 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import axios from 'axios';
-import ExploreComponent from './components/Explore.vue'; // Ensure the path is correct
-import SearchBar from './components/SearchBar.vue'; // Ensure the path is correct
+import ExploreComponent from './components/Explore.vue'; 
+import SearchBar from './components/SearchBar.vue'; 
 
 const app = createApp({});
 
-// Register components globally if needed
 app.component('explore-component', ExploreComponent);
-app.component('search-bar', SearchBar); // Register SearchBar if used in the Blade file
+app.component('search-bar', SearchBar); 
 
 app.mount('#app');
